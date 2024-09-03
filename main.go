e58db5c35226af34c850a9c8463b3c111971cfc8
Soltész Márk <mark.soltesz@shoprenter.hu>
2024-09-03 19:26:16 +0200

    editor state

--- main.go
+++ main.go
@@ -8,20 +8,55 @@ import (
 )
 
 type state struct {
-	termios unix.Termios
+	screenRows  int
+	screenCols  int
+	origTermios unix.Termios
 }
 
-var oldState *state = &state{}
+var editorState *state = &state{}
 
 func main() {
 	enterRawMode()
+	initEditor()
+
+	fmt.Println(editorState.screenRows)
 
 	for {
-		processKeypress()
+		editorClearScreen()
+		editorProcessKeypress()
 	}
 }
 
-func processKeypress() {
+func initEditor() {
+	getWindowSize(&editorState.screenRows, &editorState.screenCols)
+}
+
+func getWindowSize(rows *int, cols *int) {
+	size, err := unix.IoctlGetWinsize(int(os.Stdin.Fd()), unix.TIOCGWINSZ)
+	if err != nil {
+		panic(err)
+	}
+
+	*rows = int(size.Row)
+	*cols = int(size.Col)
+}
+
+func editorDrawRows() {
+	for y := 0; y < editorState.screenRows; y++ {
+		fmt.Fprintf(os.Stdout, "~\r\n")
+	}
+}
+
+func editorClearScreen() {
+	fmt.Fprintf(os.Stdout, "\x1b[2J")
+	fmt.Fprintf(os.Stdout, "\x1b[H")
+
+	editorDrawRows()
+
+	fmt.Fprintf(os.Stdout, "\x1b[H")
+}
+
+func editorProcessKeypress() {
 	c := readKey()
 
 	switch c {
@@ -47,7 +82,7 @@ func ctrlKey(k byte) byte {
 	return k & 0x1f
 }
 
-func enterRawMode() (*state, error) {
+func enterRawMode() error {
 	fd := int(os.Stdin.Fd())
 
 	termios, err := unix.IoctlGetTermios(fd, unix.TCGETS)
@@ -55,7 +90,7 @@ func enterRawMode() (*state, error) {
 		panic(err)
 	}
 
-	oldState = &state{termios: *termios}
+	editorState.origTermios = *termios
 
 	termios.Iflag &^= unix.IXON | unix.IEXTEN | unix.ICRNL
 	termios.Lflag &^= unix.ECHO | unix.ICANON | unix.ISIG
@@ -67,15 +102,17 @@ func enterRawMode() (*state, error) {
 
 	unix.IoctlSetTermios(fd, unix.TCSETS, termios)
 
-	return oldState, nil
+	return nil
 }
 
 func disableRawMode() {
 	fd := int(os.Stdin.Fd())
-	unix.IoctlSetTermios(fd, unix.TCSETS, &oldState.termios)
+	unix.IoctlSetTermios(fd, unix.TCSETS, &editorState.origTermios)
 }
 
 func exit(code int) {
 	disableRawMode()
+	fmt.Fprintf(os.Stdout, "\x1b[2J")
+	fmt.Fprintf(os.Stdout, "\x1b[H")
 	os.Exit(code)
 }
